f1724f854990cb44f6e6e3ec07d0e526d7e0dbfe
Quazie Kwaselow <Quazie@dropbox.com>
2016-12-06 10:51:34 -0800

    Day 6 - Much cleaner version

--- Day6/day6.rs
+++ Day6/day6.rs
@@ -11,7 +11,7 @@ use std::collections::HashSet;
 use std::collections::btree_map::BTreeMap;
 use std::cmp::Ordering;
 
-fn get_first_and_last(map: BTreeMap<char, i32>) -> (char, char) {
+fn get_first_and_last(map: &BTreeMap<char, i32>) -> (char, char) {
   let mut count_vec: Vec<(&char, &i32)> = map.iter().collect();
   count_vec.sort_by(|a, b| b.1.cmp(a.1));
 
@@ -25,54 +25,30 @@ fn main() {
     let mut s = String::new();
     f.read_to_string(&mut s);
     let lines = s.lines();
-    let mut count0 = BTreeMap::new();
-    let mut count1 = BTreeMap::new();
-    let mut count2 = BTreeMap::new();
-    let mut count3 = BTreeMap::new();
-    let mut count4 = BTreeMap::new();
-    let mut count5 = BTreeMap::new();
-    let mut count6 = BTreeMap::new();
-    let mut count7 = BTreeMap::new();
+
+    let mut map_vec = Vec::new();
+    for i in 0..8 {
+      map_vec.push(BTreeMap::new())
+    }
+
     for l in lines {
       let mut line = String::new();
       line = format!("{}", l);
-      *count7.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count6.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count5.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count4.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count3.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count2.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count1.entry(line.pop().unwrap()).or_insert(0) += 1;
-      *count0.entry(line.pop().unwrap()).or_insert(0) += 1;
+      for i in 0..8 {
+        let j = 7 - i;
+        *map_vec[j].entry(line.pop().unwrap()).or_insert(0) += 1;
+      }
+
     }
 
     let mut first = String::new();
     let mut last = String::new();
-    let mut fl = (' ', ' ');
-    fl = get_first_and_last(count0);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count1);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count2);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count3);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count4);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count5);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count6);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
-    fl = get_first_and_last(count7);
-    first += &format!("{}", fl.0);
-    last += &format!("{}", fl.1);
+    let mut f_l = (' ', ' ');
+    for i in 0..8 {
+      f_l = get_first_and_last(&map_vec[i]);
+      first += &format!("{}", f_l.0);
+      last += &format!("{}", f_l.1);
+    }
 
     println!("Part one: {:?}", first);
     println!("Part two: {:?}", last);
