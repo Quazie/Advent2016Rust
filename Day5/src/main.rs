29ec27d6a4043a5ef02f4f910460b72b099298c4
Quazie Kwaselow <Quazie@dropbox.com>
2016-12-07 10:25:35 -0800

    Day 7

--- Day5/src/main.rs
+++ Day5/src/main.rs
@@ -4,44 +4,44 @@ use crypto::md5::Md5;
 use crypto::digest::Digest;
 
 fn main() {
-    let mut password = String::new();
+  let mut password = String::new();
 
-    let mut hasher = Md5::new();
-    let key = "ugkcyxxp".as_bytes();
-    let mut password2: Vec<char> = vec![' '; 8];
-    for i in 0..u64::max_value(){
-        hasher.input(key);
-        hasher.input(i.to_string().as_bytes());
+  let mut hasher = Md5::new();
+  let key = "ugkcyxxp".as_bytes();
+  let mut password2: Vec<char> = vec![' '; 8];
+  for i in 0..u64::max_value(){
+    hasher.input(key);
+    hasher.input(i.to_string().as_bytes());
 
-        let mut output = hasher.result_str();
+    let mut output = hasher.result_str();
 
-        if output.starts_with("00000"){
-          let char7 = output.remove(6);
-          let char6 = output.remove(5);
-          let index:i32 = i32::from_str_radix(&char6.to_string(), 16).unwrap();
-          if password.len() != 8 {
-            password += &format!("{}", char6);
-            println!("Password 1 is: {:?}", password);
-          }
-          if index < 8  && password2[index as usize] == ' '{
-            password2[index as usize] = char7;
-            println!("Password 2 is: {:?}", password2);
-          }
-          let mut done = true;
-          for j in 0..8 {
-            if password2[j as usize] == ' ' {
-              done = false;
-              break;
-            }
-          }
-          if done {
-            break;
-          }
+    if output.starts_with("00000"){
+      let char7 = output.remove(6);
+      let char6 = output.remove(5);
+      let index:i32 = i32::from_str_radix(&char6.to_string(), 16).unwrap();
+      if password.len() != 8 {
+        password += &format!("{}", char6);
+        println!("Password 1 is: {:?}", password);
+      }
+      if index < 8  && password2[index as usize] == ' '{
+        password2[index as usize] = char7;
+        println!("Password 2 is: {:?}", password2);
+      }
+      let mut done = true;
+      for j in 0..8 {
+        if password2[j as usize] == ' ' {
+          done = false;
+          break;
         }
-
-        hasher.reset();
+      }
+      if done {
+        break;
+      }
     }
 
-    println!("Password 1 is: {:?}", password);
-    println!("Password 2 is: {:?}", password2);
+    hasher.reset();
+  }
+
+  println!("Password 1 is: {:?}", password);
+  println!("Password 2 is: {:?}", password2);
 }
---
+++ Day7/day7.rs
@@ -0,0 +1,70 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::VecDeque;
+
+fn get_aba_or_abba(s: &String, abba: bool) -> Vec<(char, char)>{
+  let mut aba_vec = Vec::new();
+  let mut char_vec = VecDeque::new();
+  let len = if abba {4} else {3};
+
+  for ch in s.chars() {
+    char_vec.push_back(ch);
+
+    // Only look at characters for the given pattern
+    if char_vec.len() > len {
+      char_vec.pop_front();
+    }
+
+    // if we have enough chars to look for a pattern, see if we've found the pattern
+    if char_vec.len() == len {
+      if char_vec[1] != char_vec[0]{
+        if (abba && char_vec[0] == char_vec[3] && char_vec[1] == char_vec[2]) ||
+           (!abba && char_vec[0] == char_vec[2]) {
+          aba_vec.push((char_vec[0],char_vec[1]));
+        }
+      }
+    }
+  }
+
+  return aba_vec;
+}
+
+fn main() {
+
+  let mut f = File::open("day7.txt").unwrap();
+  let mut f_s = String::new();
+  f.read_to_string(&mut f_s).unwrap();
+
+  let mut tls_count = 0; // part 1
+  let mut ssl_count = 0; // part 2
+
+  for l in f_s.lines() {
+    let mut s: String = l.to_string();
+    let mut supernet_bits = String::new();
+    let mut hypernet_bits = String::new();
+
+    while s.len() > 0 {
+      let supernet_offset = s.find('[').unwrap_or(s.len());
+      let part_str: String = s.drain(..supernet_offset).collect();
+      supernet_bits.push_str(&part_str);
+
+      let hypernet_offset = s.find(']').unwrap_or(s.len());
+      let part_str: String =  s.drain(..hypernet_offset).collect();
+      hypernet_bits.push_str(&part_str);
+    }
+
+    if get_aba_or_abba(&supernet_bits, true).len() != 0 && get_aba_or_abba(&hypernet_bits, true).len() == 0{
+      tls_count += 1;
+    }
+
+    let bab_list = get_aba_or_abba(&hypernet_bits, false);
+    for aba in get_aba_or_abba(&supernet_bits, false) {
+      if bab_list.contains(&(aba.1, aba.0)) {
+        ssl_count += 1;
+        break;
+      }
+    }
+  }
+  println!("TLS Supporting IPs: {:?}", tls_count);
+  println!("SSL Supporting IPs: {:?}", ssl_count);
+}
